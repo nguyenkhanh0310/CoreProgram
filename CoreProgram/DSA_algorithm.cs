1a4018f941bc4bb027dd3e7e5054a8bccc18e125
nguyenkhanh0310 <mykhanh03102003@gmail.com>
2024-06-12 13:02:42 +0700

    Add project files.

---
+++ CoreProgram/DSA_algorithm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreProgram
+{
+    public class DSA_algorithm
+    {
+        private BigInteger p;
+        private BigInteger q;
+        private BigInteger g;
+        private BigInteger h;
+        private BigInteger x;
+        private BigInteger y;
+
+        public DSA_algorithm()
+        {
+        }
+
+        public DSA_algorithm(BigInteger q)
+        {
+            this.q = q;
+        }
+
+        public BigInteger P
+        {
+            get { return p; }
+            set { p = value; }
+        }
+
+        public BigInteger Q
+        {
+            get { return q; }
+            set { q = value; }
+        }
+
+        public BigInteger G
+        {
+            get { return g; }
+            set { g = value; }
+        }
+
+        public BigInteger H
+        {
+            get { return h; }
+            set { h = value; }
+        }
+
+        public BigInteger X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public BigInteger Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public void generateKeyDSA(int bit)
+        {
+
+        }
+    }
+}
---
+++ CoreProgram/utilities.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace CoreProgram
+{
+    public class utilities
+    {
+        public static BigInteger mod(this BigInteger dividend, BigInteger divisor)
+        {
+            BigInteger result = dividend % divisor;
+            if (result < 0)
+            {
+                result += divisor;
+            }
+            return result;
+        }
+
+        public static BigInteger getRandomNumber(BigInteger min, BigInteger max)
+        {
+            Random random = new Random();
+            BigInteger randomBigInteger = BigInteger.ProbablePrime()
+        }
+        public static Boolean isPrime(BigInteger n, int k)
+        {
+            if(n.CompareTo(BigInteger.One) <= 0 || mod(n, new BigInteger(2)).Equals(BigInteger.Zero))
+            {
+                return false;
+            }
+
+            // Tách n = d*2^s+1
+            BigInteger d = n - 1;
+            int s = 0;
+            while (mod(d, new BigInteger(2)).Equals(BigInteger.Zero){
+                s++;
+                d = d / 2;
+            }
+
+            //Kiểm tra thuật toán với k lần lặp
+            for(int i = 0; i < k; i++)
+            {
+
+            }
+        }
+        public static BigInteger generatePrimeNumber()
+        {
+            Random random = new Random();
+
+            int value = random.Next(1000000000) + 1000000000;
+            BigInteger q = new BigInteger(value);
+
+            while()
+        }
+    }
+}
