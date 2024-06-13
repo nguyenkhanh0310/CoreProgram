be7605193cffeb9bd8ed3e94f6e60505a81cf9bb
chuanh1214 <chuthianh2202chuthianh22@gmail.com>
2024-06-13 16:03:27 +0700

    create generateKeyDSA

--- CoreProgram/DSA_algorithm.cs
+++ CoreProgram/DSA_algorithm.cs
@@ -61,9 +61,68 @@ namespace CoreProgram
             set { y = value; }
         }
 
+        //Tạo khóa với số bit lớn
         public void generateKeyDSA(int bit)
         {
-
+            q = utilities.GeneratePrimeNumber(bit - 10);
+            p = utilities.GeneratePrimeNumberIsMultiple(q, bit);
+            h = utilities.GetRandomNumber(2, p - 2);
+            do
+            {
+                g = BigInteger.ModPow(h, (p - BigInteger.One) / q, p);
+            } while (g == BigInteger.One);
+ 
+            x = utilities.GetRandomNumber(BigInteger.One, q - BigInteger.One);
+            y = BigInteger.ModPow(g, x, p);
         }
+
+        //Tạo khóa với số nguyên tố q lớn
+        public void generateKeyDSA(BigInteger q)
+        {
+            this.q = q;
+            p = utilities.GeneratePrimeNumberIsMultiple(q);
+            h = utilities.GetRandomNumber(2, p - 2);
+            do
+            {
+                g = BigInteger.ModPow(h, (p - BigInteger.One) / q, p);
+            } while (g == BigInteger.One);
+
+            x = utilities.GetRandomNumber(BigInteger.One, q - BigInteger.One);
+            y = BigInteger.ModPow(g, x, p);
+        }
+        public void generateKeyDSA()
+        {
+            q = utilities.GeneratePrimeNumber();
+            p = utilities.GeneratePrimeNumberIsMultiple(q);
+            h = utilities.GetRandomNumber(2, p - 2);
+            do
+            {
+                g = BigInteger.ModPow(h, (p - BigInteger.One) / q, p);
+            } while (g == BigInteger.One);
+
+            x = utilities.GetRandomNumber(BigInteger.One, q - BigInteger.One);
+            y = BigInteger.ModPow(g, x, p);
+        }
+
+        public static Dictionary<BigInteger, BigInteger> SigningMessage(string hashCode, BigInteger p, BigInteger q, BigInteger g, BigInteger x)
+        {
+            Dictionary<BigInteger, BigInteger> signature = new Dictionary<BigInteger, BigInteger>();
+            BigInteger hashValue = BigInteger.Parse(hashCode, System.Globalization.NumberStyles.HexNumber);
+            BigInteger k, r, s;
+
+            do
+            {
+                k = utilities.GetRandomNumber(BigInteger.One, q - BigInteger.One);
+                r = BigInteger.ModPow(g, k, p) % q;
+                s = utilities.NghichDao(k, q)* (hashValue + x * r) % q;
+            }
+            while (r == BigInteger.Zero || s == BigInteger.Zero);
+
+            signature.Add(r, s);
+
+            return signature;
+        }
+
+
     }
 }
