85be660ff8bc7f2d5c04baea13c1fe8c1b642953
chuanh1214 <chuthianh2202chuthianh22@gmail.com>
2024-06-13 10:56:51 +0700

    update program

--- CoreProgram/Program.cs
+++ CoreProgram/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CoreProgram;
+using System.Numerics;
+class Program
+{
+    public static void Main()
+    {
+        BigInteger a = 11;
+        BigInteger n = 17;
+        Console.WriteLine(utilities.NghichDao(a,n));
+        int k = 20;
+        if (utilities.IsPrime(n, k))
+        {
+            Console.WriteLine("True");
+        }
+        else
+        {
+            Console.WriteLine("False");
+        }
+        Console.WriteLine("ran = "+utilities.GetRandomNumber(2, n-1));
+        BigInteger q = utilities.GeneratePrimeNumber();
+        Console.WriteLine("q=" + q);
+
+        Console.WriteLine("p= " + utilities.GeneratePrimeNumberIsMultiple(q));
+    }
+}
